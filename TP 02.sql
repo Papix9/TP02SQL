5e9276a2c3cd6c86cc7d55e1f627985258949d1d
Lucifer <romainbassoni@gmail.com>
2025-12-10 13:11:54 +0100

    ajout du fichier SQL TP02

---
+++ TP 02.sql
@@ -0,0 +1,72 @@
+-- 1) CREATION DE LA BASE DE DONNEES
+DROP DATABASE IF EXISTS compta;
+CREATE DATABASE compta CHARACTER SET utf8mb4;
+USE compta;
+
+-- 2) CREATION DES TABLES
+
+-- TABLE FOURNISSEUR
+CREATE TABLE FOURNISSEUR (
+    ID INT PRIMARY KEY,
+    NOM VARCHAR(100) NOT NULL
+);
+
+-- TABLE ARTICLE
+CREATE TABLE ARTICLE (
+    ID INT PRIMARY KEY,
+    REF VARCHAR(20) NOT NULL,
+    DESIGNATION VARCHAR(255) NOT NULL,
+    PRIX DECIMAL(10,2) NOT NULL,
+    ID_FOU INT NOT NULL,
+    FOREIGN KEY (ID_FOU) REFERENCES FOURNISSEUR(ID)
+);
+
+-- TABLE BON
+CREATE TABLE BON (
+    ID INT PRIMARY KEY,
+    NUMERO VARCHAR(20) NOT NULL,
+    DATE_CMDE DATE NOT NULL,
+    DELAI INT NOT NULL,
+    ID_FOU INT NOT NULL,
+    FOREIGN KEY (ID_FOU) REFERENCES FOURNISSEUR(ID)
+);
+
+-- TABLE COMPO
+CREATE TABLE COMPO (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    QTE INT NOT NULL,
+    ID_ART INT NOT NULL,
+    ID_BON INT NOT NULL,
+    FOREIGN KEY (ID_ART) REFERENCES ARTICLE(ID),
+    FOREIGN KEY (ID_BON) REFERENCES BON(ID)
+);
+
+-- 3) INSERTION DES FOURNISSEURS
+INSERT INTO FOURNISSEUR (ID, NOM) VALUES
+(1, 'Française d’Imports'),
+(2, 'FDM SA'),
+(3, 'Dubois & Fils');
+
+
+-- 4) INSERTION DES ARTICLES
+INSERT INTO ARTICLE (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES
+(1, 'A01', 'Perceuse P1', 74.99, 1),
+(2, 'F01', 'Boulon laiton 4 x 40 mm (sachet de 10)', 2.25, 2),
+(3, 'F02', 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.45, 2),
+(4, 'D01', 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.40, 3),
+(5, 'A02', 'Meuleuse 125mm', 37.85, 1),
+(6, 'D03', 'Boulon acier zingué 4 x 40mm (sachet de 10)', 1.80, 3),
+(7, 'A03', 'Perceuse à colonne', 185.25, 1),
+(8, 'D04', 'Coffret mêches à bois', 12.25, 3),
+(9, 'F03', 'Coffret mêches plates', 6.25, 2),
+(10, 'F04', 'Fraises d’encastrement', 8.14, 2);
+
+-- 5) INSERTION DU BON DE COMMANDE
+INSERT INTO BON (ID, NUMERO, DATE_CMDE, DELAI, ID_FOU)
+VALUES (1, '001', CURDATE(), 3, 1);
+
+-- 6) COMPOSITION DU BON DE COMMANDE
+INSERT INTO COMPO (QTE, ID_ART, ID_BON) VALUES
+(3, 1, 1),   -- 3 perceuses P1
+(4, 5, 1),   -- 4 meuleuses 125mm
+(1, 7, 1);   -- 1 perceuse à colonne
